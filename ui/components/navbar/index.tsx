bc2ffc61f4f405c3f306aca1c948ea11ab8428b2
Vedant Chainani <vedantchainani1084@gmail.com>
2023-07-03 12:06:37 +0530

    add fonts

--- ui/components/navbar/index.tsx
+++ ui/components/navbar/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Navbar, Link, Button } from '@nextui-org/react';
 
-import { Inter } from 'next/font/google';
-const inter = Inter({ subsets: ['latin'] });
-
 const NavBar = () => {
 	return (
 		<Navbar
 			variant='sticky'
-			css={{ background: '#141414', backgroundColor: '#141414', color: '#fff' }}
+			css={{ color: '#fff' }}
 			containerCss={{
 				backgroundColor: '#141414',
 			}}
-			className='!text-white'
 			disableBlur
 			maxWidth='fluid'
 		>
-			<Navbar.Brand className={`${inter.className} text-xl font-semibold`}>
-				zkTalents
+			<Navbar.Brand>
+				<p className='text-2xl font-bold tracking-wide orkneyMedium'>
+					zkTalents
+				</p>
 			</Navbar.Brand>
 			<Navbar.Content
 				activeColor='neutral'
