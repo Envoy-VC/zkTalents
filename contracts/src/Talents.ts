303480c79c1a41ef6cc0b21a0873ed58f881ec94
Vedant Chainani <vedantchainani1084@gmail.com>
2023-07-04 18:56:19 +0530

    update contract

--- contracts/src/Talents.ts
+++ contracts/src/Talents.ts
@@ -8,17 +8,15 @@ import {
   Permissions,
   PublicKey,
   Signature,
-  MerkleMapWitness,
   AccountUpdate,
+  PrivateKey,
 } from 'snarkyjs';
 
 const ORACLE_PUBLIC_KEY = 'oracle_public_key';
-let initialRoot = Field(0);
 
 export class Talents extends SmartContract {
   @state(PublicKey) oraclePublicKey = State<PublicKey>();
   @state(Field) talentCounter = State<Field>();
-  @state(Field) mapRoot = State<Field>();
 
   init() {
     super.init();
@@ -28,78 +26,34 @@ export class Talents extends SmartContract {
     });
     this.oraclePublicKey.set(PublicKey.fromBase58(ORACLE_PUBLIC_KEY));
     this.talentCounter.set(Field(0));
-    this.mapRoot.set(initialRoot);
     this.requireSignature();
   }
-  /**
-   * @param  {Field} Talent
-   * @param  {Signature} signature
-   * @param  {MerkleMapWitness} keyWitness
-   * @param  {Field} keyToChange
-   * @param  {Field} valueBefore
-   * @param  {Field} valueAfter
-   */
-  @method addTalent(
-    Talent: Field,
-    signature: Signature,
-    keyWitness: MerkleMapWitness,
-    keyToChange: Field,
-    valueBefore: Field,
-    valueAfter: Field
-  ) {
-    // Get Oracle Public Key
-    const oraclePublicKey = this.oraclePublicKey.get();
-    this.oraclePublicKey.assertEquals(oraclePublicKey);
 
-    // Verify Signature
-    const validSignature = signature.verify(oraclePublicKey, [
-      Talent,
-      valueAfter,
-    ]);
-    validSignature.assertTrue();
-
+  @method addTalent() {
     // Check if the applicant is eligible
     let senderUpdate = AccountUpdate.createSigned(this.sender);
     senderUpdate.account.balance
       .get()
-      .assertGreaterThanOrEqual(UInt64.from(1e10));
-
-    // Get the Initial Root and and Check valueBefore
-    const initialRoot = this.mapRoot.get();
-    this.mapRoot.assertEquals(initialRoot);
-
-    valueBefore.assertEquals(Field(0));
+      .assertGreaterThanOrEqual(
+        UInt64.from(1e10),
+        'Not Eligible to Create Talents'
+      );
 
     // Increment the talent counter
     const counter = this.talentCounter.get();
     this.talentCounter.assertEquals(counter);
 
     this.talentCounter.set(counter.add(Field(1)));
-
-    // Now Update the Root before sending MINA to prevent re-entrancy attacks
-    const [rootBefore, key] = keyWitness.computeRootAndKey(valueBefore);
-    rootBefore.assertEquals(initialRoot);
-
-    key.assertEquals(keyToChange);
-
-    const [rootAfter, _] = keyWitness.computeRootAndKey(valueAfter);
-    this.mapRoot.set(rootAfter);
   }
   /**
+   * @param  {PublicKey} pb
    * @param  {Field} isEligible
    * @param  {Signature} signature
-   * @param  {MerkleMapWitness} keyWitness
-   * @param  {Field} keyToChange
-   * @param  {Field} valueBefore
-   * @param  {Field} valueAfter
    */
   @method applyToTalent(
+    pb: PublicKey,
     isEligible: Field,
-    signature: Signature,
-    keyWitness: MerkleMapWitness,
-    keyToChange: Field,
-    valueBefore: Field,
-    valueAfter: Field
+    signature: Signature
   ) {
     // Get Oracle Public Key
     const oraclePublicKey = this.oraclePublicKey.get();
@@ -108,26 +62,11 @@ export class Talents extends SmartContract {
     // Verify Signature
     const validSignature = signature.verify(oraclePublicKey, [
       isEligible,
-      valueAfter,
+      Field.fromFields(pb.toFields()),
     ]);
     validSignature.assertTrue();
 
     // Check if the applicant is eligible
-    isEligible.assertEquals(Field(1));
-
-    // Get the Initial Root and and Check valueBefore
-    const initialRoot = this.mapRoot.get();
-    this.mapRoot.assertEquals(initialRoot);
-
-    valueBefore.assertEquals(Field(0));
-
-    // Now Update the Root before sending MINA to prevent reentrancy attacks
-    const [rootBefore, key] = keyWitness.computeRootAndKey(valueBefore);
-    rootBefore.assertEquals(initialRoot);
-
-    key.assertEquals(keyToChange);
-
-    const [rootAfter, _] = keyWitness.computeRootAndKey(valueAfter);
-    this.mapRoot.set(rootAfter);
+    isEligible.assertEquals(Field(1), 'Not Eligible');
   }
 }
