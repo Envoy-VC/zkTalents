3bc4c97bebebf47b7815a3f10149b0e93b014917
peteruche21 <anyaogupeter601@gmail.com>
2023-07-05 12:22:10 +0100

    chore: test Talent

--- contracts/src/Talents.ts
+++ contracts/src/Talents.ts
@@ -9,14 +9,24 @@ import {
   PublicKey,
   Signature,
   AccountUpdate,
+  PrivateKey,
+  Struct,
 } from 'snarkyjs';
 
-const ORACLE_PUBLIC_KEY =
-  'B62qkyP8a2RfB5dZbdoRSWQqmbsdkTNWA8aDWNWt8ocndZhL7qqtgFD';
-
+export const ORACLE_PUBLIC_KEY =
+  'B62qp2oQ8LS4qzXAhQXBrurVowJpEwD4coYvejU4FvZGVDeFf1AMRES';
+class ApplyTalent extends Struct({
+  pb: PublicKey,
+  eligibilityScore: Field,
+}) {}
 export class Talents extends SmartContract {
   @state(PublicKey) oraclePublicKey = State<PublicKey>();
   @state(Field) talentCounter = State<Field>();
+
+  events = {
+    'add-talent': Field,
+    'apply-talent': ApplyTalent,
+  };
 
   init() {
     super.init();
@@ -32,6 +42,11 @@ export class Talents extends SmartContract {
   @method addTalent() {
     // Check if the applicant is eligible
     let senderUpdate = AccountUpdate.createSigned(this.sender);
+
+    senderUpdate.account.balance.assertEquals(
+      senderUpdate.account.balance.get()
+    );
+
     senderUpdate.account.balance
       .get()
       .assertGreaterThanOrEqual(
@@ -43,8 +58,12 @@ export class Talents extends SmartContract {
     const counter = this.talentCounter.get();
     this.talentCounter.assertEquals(counter);
 
-    this.talentCounter.set(counter.add(Field(1)));
+    const newCount = counter.add(Field(1));
+
+    this.talentCounter.set(newCount);
+    this.emitEvent('add-talent', newCount);
   }
+
   /**
    * @param  {PublicKey} pb
    * @param  {Field} isEligible
@@ -52,21 +71,27 @@ export class Talents extends SmartContract {
    */
   @method applyToTalent(
     pb: PublicKey,
-    isEligible: Field,
+    eligibilityScore: Field,
     signature: Signature
   ) {
+    // eligibilityScore should be the average of the requirements score normalised to a 100 mark
+
     // Get Oracle Public Key
     const oraclePublicKey = this.oraclePublicKey.get();
     this.oraclePublicKey.assertEquals(oraclePublicKey);
 
+    const pbToField = Field.fromFields(pb.toFields());
     // Verify Signature
     const validSignature = signature.verify(oraclePublicKey, [
-      isEligible,
-      Field.fromFields(pb.toFields()),
+      pbToField,
+      eligibilityScore,
     ]);
     validSignature.assertTrue();
 
     // Check if the applicant is eligible
-    isEligible.assertEquals(Field(1), 'Not Eligible');
+    eligibilityScore.assertGreaterThanOrEqual(Field(50), 'Not Eligible');
+    this.emitEvent('apply-talent', { pb, eligibilityScore });
   }
+
+  // TODO merkle map that keeps track of applications
 }
